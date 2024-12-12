1d92d7187366a6d17b95b932706d595a8a4c5eb1
Diogo01SC <diogodsc2001@hotmail.com>
2024-12-12 20:29:49 -0300

    adicionando alert

--- script.js
+++ script.js
@@ -2,18 +2,27 @@ const clickButton = document.querySelector(".button-raffle")
 const resultNumber = document.querySelector(".text-result")
 
 
-function generateNumber(){
- 
-const min = Math.ceil (document.querySelector(".input-min").value)
-const max = Math.floor (document.querySelector(".input-max").value)
+function generateNumber() {
 
-const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    const min = Math.ceil(document.querySelector(".input-min").value)
+    const max = Math.floor(document.querySelector(".input-max").value)
 
-resultNumber.innerHTML = result
+    if (min >= max) {
+        resultNumber.innerHTML = "0"
+
+        alert("O número digitado no primeiro quadro, não pode ser MAIOR que o segundo quadro!!!")
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+        resultNumber.innerHTML = result
+    }
+
+
+
 
 }
 
 
 
-clickButton.addEventListener("click", generateNumber )
+clickButton.addEventListener("click", generateNumber)
 
